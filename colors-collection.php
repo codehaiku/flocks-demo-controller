40a00764a8425fec48dd090b34825a5d9bdd727c
Joseph Gabito <codehaiku.io@gmail.com>
2017-01-17 23:20:33 +0800

    Initial Commit

---
+++ colors-collection.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Returns all the background color;
+ * 
+ * @return [type] [description]
+ */
+function fdc_background_color_collecton() {
+
+	$selectors = ".background-primary, #header-menu-toggle:hover .menu-bar, #header-menu-toggle.active:hover .menu-bar, .nav-links .nav-previous a:hover, .nav-links .nav-next a:hover, .entry-footer .edit-link a, #colophon.site-footer, .edit-link a, button, input[type=button], input[type=submit], .button, .subscription-toggle, #main-menu ul#main-menu-ul li.menu-item > ul.sub-menu li.menu-item a:hover, .error-404.not-found .page-header .not-found-btn, .error-404.not-found .page-content .searchform label .searchsubmit, .dunhakdis-headings:after, article table th, #user-navigation ul#nav-user-action-notification > li:before, .widget.widget_tag_cloud > .tagcloud a:hover, .widget .widget-title:after, .custom .tp-bullet:hover, .custom .tp-bullet.selected, .gears-button.solid.primary, .gears-bp-groups-carousel-2 .member-count, .gears-recent-posts .gears-article-recent-posts:hover .entry-post-format-icon, body .vc_btn3.vc_btn3-color-blue, body .vc_btn3.vc_btn3-color-blue.vc_btn3-style-flat, .flocks-vc-row-container .vc_toggle_active.vc_toggle_size_md.vc_toggle_simple .vc_toggle_icon, .gears-pricing-table.popular .gears-pricing-table-price-label, span.gears-dropcap-letter.circular,";
+
+	$selectors .=  ".background-primary, .bp_button, .buddypress.groups .dir-form .list-style-container ul li#group-create-nav a, .buddypress.groups .bp-objects-loop li .action .meta, .buddypress .bp-objects-loop li .generic-button a, .buddypress .bp-objects-loop li .action .button, .bp-user #item-body .groups.mygroups #group-create-nav a, .single-item.groups .bp-objects-loop.type-grid .generic-button a, .buddypress.members .bp-objects-loop.members-list li .item .item-title .item-update .activityreadmore a, .buddypress .pagination .pagination-links .page-numbers.current, .buddypress .pagination .pagination-links .page-numbers:hover, .buddypress .item-list-tabs ul li a span, .buddypress ul#activity-stream .load-newest a, .buddypress ul#activity-stream .load-more a, .bp-user #item-body .bp-objects-loop.members-list li .item .item-title .item-update .activityreadmore a, .bp-user #item-body .pagination .pagination-links .page-numbers.current, .bp-user #item-body .pagination .pagination-links .page-numbers:hover, #cv-cover-photo #cv-cover-photo-elements #item-buttons a, .single-item.groups #group-settings-form #request-list li .action .generic-button a, .widget_bp_groups_widget .item-options a.selected, .widget_bp_core_friends_widget .item-options a.selected, .widget_bp_core_members_widget .item-options a.selected, .buddypress.register #signup_form #profile-details-section h4:after, .buddypress.register #signup_form #basic-details-section h4:after,";
+
+	$selectors .= "#user-navigation ul li a .notification-count, #user-navigation ul li a .cart-count";
+
+	return $selectors;
+}
+
+
+function fdc_color_collection() {
+
+
+	$selectors = ".color-primary, .header-style-3 #main-menu ul#main-menu-ul li.menu-item a:hover, .header-style-4 #main-menu ul#main-menu-ul li.menu-item a:hover, .header-style-3 #main-menu ul#main-menu-ul li.menu-item.menu-item-has-children > a:hover:after, .header-style-4 #main-menu ul#main-menu-ul li.menu-item.menu-item-has-children > a:hover:after, .header-style-3.logged-out #main-menu .col-lg-2.col-md-2 #user-navigation ul li:first-of-type a:hover, .header-style-3.logged-out #main-menu .col-lg-2.col-md-2 #user-navigation ul li:nth-child(2) a:hover, .header-style-4.logged-out #main-menu .col-lg-2.col-md-2 #user-navigation ul li:first-of-type a:hover, .header-style-4.logged-out #main-menu .col-lg-2.col-md-2 #user-navigation ul li:nth-child(2) a:hover, #comments h2.comments-title p span, #comments .comment-meta .comemnt-date:hover, #footer-style-3-social-media-list .item a:hover, #main-menu ul#main-menu-ul li.menu-item.menu-item-has-children > a:after, .error-404.not-found .page-header .page-title, .error-404.not-found .page-header h3, .error-404.not-found .page-header p, .single #flocks-share-count, a, a:focus, a:hover, #user-navigation ul#nav-user-action-notification li a:hover, #user-navigation ul#nav-user-action-dropdown li a:hover, .gears-testimonial-carousel .gears-testimonials-item .gears-testimonials-author .gears-testimonial-author-name, #bbpress-forums div.bbp-template-notice a, #bbpress-forums div.bbp-template-notice.info a, #bbpress-forums div.bbp-template-notice.info:before, .gears-login-wrap .login-password label:after, .gears-login-wrap .login-username label:after,";
+
+	$selectors .= ".color-primary, .woocommerce ul.products li.product a:hover, .woocommerce ul.products li.product .price, .woocommerce .widget_price_filter .price_slider_amount .button, .woocommerce-thankyou-order-received, .woocommerce .woocommerce-info:before";
+
+	return $selectors;
+
+}
---
+++ flocks-demo-controller.php
@@ -0,0 +1,237 @@
+<?php
+/*
+Plugin Name: Flocks Demo Controller
+Plugin URI:  https://dunhakdis.com/
+Description: Allows visitors to select colors, layout, and other awesome things!
+Version:     1.0
+Author:      Dunhakdis Software Creatives
+Author URI:  https://dunhakdis.com/
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: dunhakdis
+Domain Path: /languages
+*/
+
+require_once plugin_dir_path(__DIR__) . 'flocks-demo-controller/colors-collection.php';
+
+add_action('wp_footer', 'fdc_render_view');
+
+
+function fdc_render_view() { ?>
+<style>
+
+#flocks-demo-controller-view {
+	position: fixed;
+	right: -17px;
+	top: calc( 50% - 250px );
+	background: #fff;
+	border: 1px solid #eee;
+	z-index: 999999;
+	box-shadow: 0px 10px 25px -15px #607D8B;
+
+	transform: translateX(235px);
+
+	transition: all 0.25s ease-in-out;
+	-webkit-transition: all 0.25s ease-in-out;
+	-moz-transition: all 0.25s ease-in-out;
+}
+
+#flocks-demo-controller-view.active {
+	transform: translateX(0px);
+}
+
+#flocks-demo-controller-controls {
+	padding: 20px;
+	width: 250px;
+	min-height: 250px;
+	z-index: 2000;
+    position: relative;
+    background: #fff;
+    border-right: 0;
+}
+
+#flocks-demo-controller-toggle {
+	position: absolute;
+    top: calc( 50% - 30px );
+    left: -60px;
+    height: 60px;
+    width: 60px;
+    text-align: center;
+    padding-top: 8px;
+    font-size: 26px;
+    background: white;
+    border: 1px solid #eee;
+    border-right: 0;
+    z-index: 1000;
+    box-shadow: 19px 6px 25px -10px #020202;
+    color: #292f32;
+    cursor: pointer;
+    border-top-left-radius: 4px;
+    border-bottom-left-radius: 4px;
+}
+
+#flocks-demo-controller-color-schemes {
+	padding: 0;
+	margin: 15px 0;
+}
+#flocks-demo-controller-color-schemes li {
+	padding: 0;
+	margin: 0;
+	list-style: none;
+	display: inline-block;
+	width: 30px;
+	height: 30px;
+	background: red;
+	border-radius: 50%;
+	cursor: pointer;
+	transition: all 0.15s ease-in-out;
+	-webkit-transition: all 0.15s ease-in-out;
+	-moz-transition: all 0.15s ease-in-out;
+}
+
+#flocks-demo-controller-color-schemes li.red {background: #f44336;}
+#flocks-demo-controller-color-schemes li.purple {background: #e91e63;}
+#flocks-demo-controller-color-schemes li.indigo {background: #9c27b0;}
+#flocks-demo-controller-color-schemes li.violet {background: #673ab7;}
+#flocks-demo-controller-color-schemes li.blue {background: #3f51b5;}
+#flocks-demo-controller-color-schemes li.light-blue {background: #2196f3;}
+#flocks-demo-controller-color-schemes li.sky-blue {background: #03a9f4;}
+#flocks-demo-controller-color-schemes li.blue-green {background: #00bcd4;}
+#flocks-demo-controller-color-schemes li.green {background: #009688;}
+#flocks-demo-controller-color-schemes li.dark-green {background: #4caf50;}
+#flocks-demo-controller-color-schemes li.light-green {background: #8bc34a;}
+#flocks-demo-controller-color-schemes li.yellow-green {background: #cddc39;}
+#flocks-demo-controller-color-schemes li.main-blue {background: #22b5ce;}
+#flocks-demo-controller-color-schemes li.orange {background: #ffc107;}
+#flocks-demo-controller-color-schemes li.dark-orange {background: #ff9800;}
+#flocks-demo-controller-color-schemes li.red-orange {background: #ff5722;}
+#flocks-demo-controller-color-schemes li.brown {background: #795548;}
+#flocks-demo-controller-color-schemes li.teal {background: #607d8b;}
+
+#flocks-demo-controller-color-schemes li:hover,
+#flocks-demo-controller-color-schemes li.selected {
+	border: 10px double rgba(0,0,0,0.30);
+}
+
+#flocks-demo-controller-color-schemes li:active {
+	opacity: 0.85;
+	box-shadow: inset 0px 0px 7px -1px #151515;
+}
+
+#flocks-demo-controller-layout {
+	width: 95%;
+	padding: 10px;
+}
+
+#main-demo-button {
+	padding: 15px 25px;
+	text-transform: uppercase;
+	font-weight: bold;
+}
+
+.flocks-demo-controller-section h5 {
+	font-size: 10px;
+	font-weight: bold;
+	text-transform: uppercase;
+}
+</style>
+<div id="flocks-demo-controller-view">
+	<div id="flocks-demo-controller-toggle" class="background-primary">
+		<i class="fa fa-cog"></i>
+	</div>
+	<div id="flocks-demo-controller-controls">
+		<!-- color scheme -->
+		<section id="" class="flocks-demo-controller-section">
+			<h5>Color Scheme</h5>
+			<ul id="flocks-demo-controller-color-schemes">
+
+				<li data-color="22b5ce" class="selected main-blue"></li>
+				<li data-color="f44336" class="red"></li>
+				<li data-color="e91e63" class="purple"></li>
+				<li data-color="9c27b0" class="indigo"></li>
+				<li data-color="673ab7" class="violet"></li>
+				<li data-color="3f51b5" class="blue"></li>
+				<li data-color="2196f3" class="light-blue"></li>
+				<li data-color="03a9f4" class="sky-blue"></li>
+				<li data-color="00bcd4" class="blue-green"></li>
+				<li data-color="009688" class="green"></li>
+				<li data-color="4caf50" class="dark-green"></li>
+				<li data-color="8bc34a" class="light-green"></li>
+				<li data-color="cddc39" class="yellow-green"></li>
+				<li data-color="ffc107" class="orange"></li>
+				<li data-color="ff9800" class="dark-orange"></li>
+				<li data-color="ff5722" class="red-orange"></li>
+				<li data-color="795548" class="brown"></li>
+				<li data-color="607d8b" class="teal"></li>
+			</ul>
+
+		</section>
+
+
+		<!-- Demos -->
+		<section id="" class="flocks-demo-controller-section">
+			<h5>Layout</h5>
+			<select id="flocks-demo-controller-layout">
+				<option value="wide">Wide</option>
+				<option value="boxed">Boxed</option>
+			</select>
+		</section>
+
+		<!-- Main Demo -->
+		<section id="" class="flocks-demo-controller-section">
+			<h5>Main Demo</h5>
+			<a class='button' id="main-demo-button" href="#">
+				Go Back to Main Demo
+			</a>
+			
+		</section>
+	</div>
+</div>
+<style id="shityow"></style>
+<script>
+jQuery( document ).ready( function($) {
+
+	'use strict';
+
+	$('#flocks-demo-controller-color-schemes li').on('click', function(e){
+
+		var $color = '#'+$(this).data('color');
+
+		var $css = "";
+
+		$('#flocks-demo-controller-color-schemes li').removeClass('selected');
+
+		$(this).addClass('selected');
+
+		// Backgrounds.
+		$css += "<?php echo fdc_background_color_collecton(); ?> { background-color: "+$color+";}";
+
+		// Colors.
+		$css += "<?php echo fdc_color_collection(); ?> { color: "+$color+";}";
+		
+		$('#shityow').html( $css );
+
+	});
+
+	$('#flocks-demo-controller-toggle').on('click', function(e){
+
+		$('#flocks-demo-controller-view').toggleClass('active');
+
+	});
+
+	$('#flocks-demo-controller-layout').on('change', function() {
+
+		if ( "boxed" === $(this).val() ) {
+
+			$('body').addClass( "boxed" );
+
+		} else {
+
+			$('body').removeClass( "boxed" );
+
+		}
+		
+	});
+});	
+</script>
+<?php } ?>
